72d1dbf0e24a75e8f4bbeec3fb6f183403eadea7
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-01-30 10:32:16 +0545

    added fzf scripts

--- zsh/.config/zsh/keybindings.zsh
+++ zsh/.config/zsh/keybindings.zsh
@@ -19,29 +19,12 @@ stty -ixon  # Disable XON/XOFF flow control
 # Tmux-sessionizer
 bindkey -s '^t' 'tmux-sessionizer\n'
 
-# Select a file, cd into it's parent directory open the file in vim
-fzf_file(){
-  file=$(find . -type f -not -path '*/.git/*' -not -path '*/node_modules/*' -not -path '*/.venv/*' | fzf)
-  if [[ -n "$file" ]]; then
-    full_path=$(realpath "$file")
-    cd "$(dirname "$full_path")"
-    v "$full_path"
-  fi
-}
+# Sourcing my fzf scripts
+source $HOME/.local/scripts/fzf-custom
 
-# bindkey -s '^f' 'file=$(fzf) && [[ -n "$file" ]] && full_path=$(realpath "$file") && cd "$(dirname "$full_path")" && vim "$full_path"\n'
 bindkey -s '^f' 'fzf_file\n'
-
-# Search for directory and cd into it
-fzf_dir(){
-  dir=$(find . -type d -not -path '*/.git/*' -not -path '*/node_modules/*' -not -path '*/.venv/*' | fzf)
-  if [[ -n "$dir" ]]; then
-    cd "$dir"
-  fi
-}
-
-# bindkey -s '^h' '^ucd "$(find . -type d | fzf)"\n'
 bindkey -s '^h' 'fzf_dir\n'
+bindkey -s '^v' 'fzf_session\n'
 
 # Search history with text that is already on prompt and put cursor at the end
 autoload -U history-search-end
