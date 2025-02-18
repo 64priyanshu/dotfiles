1fbb99a4ff3dd5da935f1ffcee8c584d53f8a07f
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-02-18 18:41:12 +0545

    emacs keymap guide in zsh

--- zsh/.config/zsh/keybindings.zsh
+++ zsh/.config/zsh/keybindings.zsh
@@ -2,16 +2,25 @@
 bindkey -e
 
 # Some Basic EMACS motion keymaps
+# #Cursor Movement
+# CTRL-A - Move the cursor to the beginning of the line
+# CTRL-E - Move the cursor to the end of the line
+# ALT-B  - Move cursor Backward 1 word
+# ALT-F  - Move cursor Forward 1 word
+# CTRL+B - Move cursor Backward one character
+# CTRL+F - Move cursor Forward one character
+
+# #History navigation
 # CTRL-P - Go to the Previous command in your history
 # CTRL-N - Go to the Next command in your history
 # CTRL-R - Reverse-search through your history
 # CTRL-S - Search forward through your history
-# CTRL-A - Move the cursor to the beginning of the line
-# CTRL-E - Move the cursor to the end of the line
-# CTRL-W - Delete a Word backwards
-# ALT-D  - Delete a word forwards
-# ALT-F  - Move cursor Forward 1 word
-# ALT-B  - Move cursor Backward 1 word
+
+# #Editing
+# CTRL-W - Delete a word Backward/Before the cursor
+# ALT-D  - Delete a word Forward/After the cursor
+# CTRL-K - Delete everything from cursor to end of line
+# CTRL-U - Delete everything from cursor to beginning of line
 
 # Disable CTRL+S and CTRL + Q (freeze and unfreeze terminal output)
 stty -ixon  # Disable XON/XOFF flow control
@@ -22,7 +31,7 @@ bindkey -s '^t' 'tmux-sessionizer\n'
 # Sourcing my fzf scripts
 source $HOME/.local/scripts/fzf-custom
 
-bindkey -s '^f' 'fzf_file\n'
+bindkey -s '^o' 'fzf_file\n'
 bindkey -s '^h' 'fzf_dir\n'
 bindkey -s '^v' 'fzf_session\n'
 
